05befe0379fc7f4587ed61019effe0a84a908f30
shivansh1907 <dinku.cool19@gmail.com>
2025-04-07 02:03:32 +0530

    dessigning page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
-import { useState } from 'react'
+ 
+import { languages } from "./languages"
 
 
-function App() {
+export default function App() {
+
+  const langs=languages.map((lang)=>{
+   const styles={
+      backgroundColor:lang.backgroundColor,
+      color:lang.color
+    }
+    return(
+      <span className="chips" style={styles}>{lang.name}</span>
+
+    )
+
+     
+  })
+  return (
+    
+  <main>
+   <header>
+    <h1>Assembly:Endgame</h1>
+    <p>Guess the word within 8 attempts to keep the programming world safe from assembly!</p>
+   </header>
+
+   <div className="status">
+    <h3>You Win!</h3>
+    <p>Well Done!</p>
+   </div>
+
+   <div className="languages">
+    {langs}
+
+   </div>
+   
+
+  </main>
+  )
   
 }
 
-export default App
