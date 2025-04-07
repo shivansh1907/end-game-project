27192368a60a1e107e7b481e916dc896d0620d59
shivansh1907 <dinku.cool19@gmail.com>
2025-04-07 20:22:21 +0530

    designing layout of the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
  
 import { languages } from "./languages"
+import React from "react"
 
 
 export default function App() {
+  const alphabets="abcdefghijklmnopqrstuvwxyz"
+
+  const [currword,setcurrword]=React.useState("react")
+  const [guessedletters,setguessedletters]=React.useState([])
+  console.log(guessedletters)
+  //function to handle click
+  const handleclick=(e)=>{
+    const newitem=e.currentTarget.innerText
+    setguessedletters(prevletters=>[...prevletters,newitem])
+
+  }
+
 
   const langs=languages.map((lang)=>{
    const styles={
@@ -16,6 +29,20 @@ export default function App() {
 
      
   })
+
+  //functionn to handle click
+  
+  const words=currword.split("").map((word,index)=>{
+    return (
+      <span key ={index} className="letter">{word.toUpperCase()}</span>
+    )
+  })
+
+  const keyboard=alphabets.split("").map((key)=>{
+    return(
+      <button onClick={handleclick} className="key">{key.toUpperCase()}</button>
+    )
+  })
   return (
     
   <main>
@@ -24,15 +51,29 @@ export default function App() {
     <p>Guess the word within 8 attempts to keep the programming world safe from assembly!</p>
    </header>
 
-   <div className="status">
+   <section className="status">
     <h3>You Win!</h3>
     <p>Well Done!</p>
-   </div>
+   </section>
 
-   <div className="languages">
+   <section className="languages">
     {langs}
 
+   </section>
+
+   <div className="words">
+    {words}
    </div>
+
+   <div className="keyboard">
+    {keyboard}
+    
+   </div>
+
+   <div className="new">
+   <button>New Game!</button>
+   </div>
+
    
 
   </main>
